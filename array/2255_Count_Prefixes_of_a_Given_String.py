0d281ee223987fd6ee371a84b2ee5e4b3085f6af
PrantaChakraborty <cbpranta@gmail.com>
2022-05-12 16:23:09 +0600

    add count prefix

---
+++ array/2255_Count_Prefixes_of_a_Given_String.py
@@ -0,0 +1,13 @@
+"""
+link: https://leetcode.com/problems/count-prefixes-of-a-given-string/
+"""
+from typing import List
+
+
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        count = 0
+        for item in words:
+            if s.startswith(item):
+                count += 1
+        return count
